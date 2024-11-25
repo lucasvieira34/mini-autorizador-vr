ecda1dced1719e9fa5caa7109703931d1650222e
Lucas Teixeira Vieira <lucas.vieira@allos.co>
2024-11-25 18:44:55 -0300

    - feat: inicializando projeto base.

---
+++ src/main/java/br/com/lucas/vieira/miniautorizadorvr/MiniAutorizadorVrApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucas.vieira.miniautorizadorvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniAutorizadorVrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniAutorizadorVrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lucas/vieira/miniautorizadorvr/MiniAutorizadorVrApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucas.vieira.miniautorizadorvr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniAutorizadorVrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
